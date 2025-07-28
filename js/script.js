a3f6e438cbf79d718cc5f81b41bace09fbbe4c83
Arlan <arlan.oliveira022@gmail.com>
2025-07-28 17:17:07 -0300

    links prontos

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
-gsap.from('.img .nome', {
-	scrollTrigger: '.nome', // start the animation when ".box" enters the viewport (once)
-	x: -500,
-    delay: 1,
-    duration:1,
-    rotate: 0,
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.from(".container-sobre", {
     opacity:0,
-    ease:"power3.out"
+    y:50,
+    scrollTrigger:{
+        trigger:".paragrafo-sobre",
+        scrub:1,
+        start:"top center",
+        end:"top center",
+    }
+})
+
+
+let elementScrol = document.querySelector('.scroll_container')
+
+let elementContainer = elementScrol.querySelector('.Skilss')
+
+let elementFilho = Array.from(elementContainer.children);
+
+elementFilho.forEach(item => {
+    let elementDouble = item.cloneNode(true);
+    elementContainer.appendChild(elementDouble);
+    console.log(item);
+
+})
+
+document.querySelectorAll("button").forEach((botao) =>{
+  botao.disabled = true;
+})
+
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.nome',
+    start: 'top 50%', // pode ajustar
+  }
 });
-gsap.from( '.sub .s2',{
-    scrollTrigger: '.s2',
-    y:-400,
-    delay:1.8,
-    duration:1.1,
-    opacity:0,
-    ease: "elastic.out(0.8, 0.7)"
 
-})
-
-gsap.from( '.sub .s1',{
-    scrollTrigger: '.s1',
-    x:100,
-    delay:2.5,
-    duration:1.1,
-    opacity:0,
-    ease: "elastic.out(0.8, 0.7)"
-    
-})
+tl.from('.img .nome', { x: -500,rotate:90 , opacity: 0, duration: 1, ease: 'power3.out' })
+  .from('.s2', { y: 50, opacity: 0, duration: 1.1, ease: 'elastic.out(0.8, 0.5)' }, "+=0.3")
+  .from('.s1', { x: 100, opacity: 0, duration: 1.1, ease: 'elastic.out(0.8, 0.7)' }, "+=0.2");
 
 
-
-gsap.from(".sobre", {
-    scrollTrigger: {
-    trigger:".sobre",
-    start: "center center",
-    toggleActions: "restart pause reverse none"
-    },
-    y:300,
-    duration:3
-
-})
+  
